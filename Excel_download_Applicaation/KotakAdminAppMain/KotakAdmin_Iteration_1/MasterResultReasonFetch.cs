24a9ff62cf36ca0b62dfea8faf891162dc8dac35
Logeshwari <87358981+LogeshwariJSN@users.noreply.github.com>
2021-10-18 19:09:56 +0530

    Application

---
+++ Excel_download_Applicaation/KotakAdminAppMain/KotakAdmin_Iteration_1/MasterResultReasonFetch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KMBLNetNbanking
+{
+    public class MasterResultReasonFetch
+    {
+        //Result Reason Master data Fetch
+
+        public string Result_Reason { get; set; }
+
+    }
+}
